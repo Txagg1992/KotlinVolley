e2383604361bccec8fa4088f2a9d3045e3648cda
txagg1992 <curiousaps@gmail.com>
2020-01-20 20:24:54 -0600

    Add json function. Add jsonObject function.

---
+++ app/src/main/java/com/curiousca/kotlinvolley/Constant.kt
@@ -0,0 +1,4 @@
+package com.curiousca.kotlinvolley
+
+val url ="https://pixabay.com/api/?key=9900098-8e28a903a63da19e7d8c5ab7a&q=australian shepherd&image_type=photo&pretty=true"
+val url2 ="https://jsonplaceholder.typicode.com/users"
--- app/src/main/java/com/curiousca/kotlinvolley/MainActivity.kt
+++ app/src/main/java/com/curiousca/kotlinvolley/MainActivity.kt
@@ -7,14 +7,17 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     private var volleyRequest: RequestQueue? = null
-    private val url = "https://pixabay.com/api/?key=9900098-8e28a903a63da19e7d8c5ab7a&q=nude+female&image_type=photo&pretty=true"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,26 +25,76 @@ class MainActivity : AppCompatActivity() {
 
         volleyRequest = Volley.newRequestQueue(this)
 
-        getString(url)
+        //parseJson(url)
+        parseArray(url2)
+        //getString(url)
     }
 
-    
+    //This is for a json object that has an array of objects
+    private fun parseJson(Url: String) {
 
-    private fun getString(Url: String){
+        //val url ="https://pixabay.com/api/?key=9900098-8e28a903a63da19e7d8c5ab7a&q=nude+female&image_type=photo&pretty=true"
 
-        val stringRequest = StringRequest(Request.Method.GET, Url, Response.Listener {
-            response: String ->
-            try {
-                Log.d("Response", response.toString())
-            }catch (ex: Exception){
-                ex.printStackTrace()
-            }
-        },
-            Response.ErrorListener {
-                error: VolleyError ->
+        val request = JsonObjectRequest(Request.Method.GET, Url, null,
+            Response.Listener { response ->
+                try {
+                   val jsonArray: JSONArray = response.getJSONArray("hits")
+                    for (i in 0 until jsonArray.length()){
+                        val hit: JSONObject = jsonArray.optJSONObject(i)
+                        var creatorName: String = hit.getString("user")
+                        var imageUrl: String = hit.getString("webformatURL")
+                        var likecount: Int = hit.getInt("likes")
+                        var imageTags: String = hit.getString("tags")
+
+                        //Log.d("Array Entry " + i, jsonArray.toString())
+                        Log.d("Array Entry $i", creatorName + "\n" + imageTags + "\n" + imageUrl)
+                    }
+                }catch (e: JSONException){e.printStackTrace()}
+            },
+            Response.ErrorListener { error: VolleyError? ->
+                try {
+                    Log.d("XXX ERROR XXX", error.toString())
+
+                }catch (e: JSONException){e.printStackTrace()}
+            })
+        volleyRequest!!.add(request)
+
+    }
+
+    //This is for a json Array of objects
+    private fun parseArray(Url: String){
+
+        val jsonArray = JsonArrayRequest(Request.Method.GET, Url, null,
+            Response.Listener { response: JSONArray ->
+                try {
+                    Log.d("<---- Response ---->", response.toString())
+
+                }catch (e: JSONException){
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener {  })
+
+        volleyRequest!!.add(jsonArray)
+
+    }
+
+
+    //This is a simple string request that returns the entire json array
+    private fun getString(Url: String) {
+
+        val stringRequest = StringRequest(Request.Method.GET, Url,
+            Response.Listener { response: String ->
+                try {
+                    Log.d("Response", response.toString())
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+            },
+            Response.ErrorListener { error: VolleyError ->
                 try {
                     Log.d("@@Error@@", error.toString())
-                }catch (e: JSONException){
+                } catch (e: JSONException) {
                     e.printStackTrace()
                 }
             })
