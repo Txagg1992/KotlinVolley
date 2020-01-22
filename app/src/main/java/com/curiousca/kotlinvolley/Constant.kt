34de24f97a6c325983dc8e5b34da94614d687154
Txagg1992 <curiousaps@gmail.com>
2020-01-22 01:19:51 -0600

    Add new Constant Url. Add parse jsonArray. Add parse jsonObject.

--- app/src/main/java/com/curiousca/kotlinvolley/Constant.kt
+++ app/src/main/java/com/curiousca/kotlinvolley/Constant.kt
@@ -2,3 +2,4 @@ package com.curiousca.kotlinvolley
 
 val url ="https://pixabay.com/api/?key=9900098-8e28a903a63da19e7d8c5ab7a&q=australian shepherd&image_type=photo&pretty=true"
 val url2 ="https://jsonplaceholder.typicode.com/users"
+val urlQuake = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson"
--- app/src/main/java/com/curiousca/kotlinvolley/MainActivity.kt
+++ app/src/main/java/com/curiousca/kotlinvolley/MainActivity.kt
@@ -25,21 +25,58 @@ class MainActivity : AppCompatActivity() {
 
         volleyRequest = Volley.newRequestQueue(this)
 
-        //parseJson(url)
-        parseArray(url2)
+        parseBigJsonObj(urlQuake)
+        //parseJsonObject(url)
+        //parseJsonArray(url2)
         //getString(url)
     }
 
+    private fun parseBigJsonObj(Url: String) {
+        val jsonBigRequest = JsonObjectRequest(Request.Method.GET, Url, null,
+            Response.Listener { response: JSONObject ->
+                try {
+
+                    val metaObject = response.getJSONObject("metadata")
+                    var title = metaObject.getString("title")
+                    // val type = response.getString("type")
+                    //Get features json array
+                    val features: JSONArray = response.getJSONArray("features")
+                    for (i in 0 until features.length()){
+                        val propertyObj:JSONObject = features.getJSONObject(i).getJSONObject("properties")
+                        var place: String = propertyObj.getString("place")
+                        Log.d("Place: ", place)
+                        //Geometry Object
+                        val geometryObj: JSONObject = features.getJSONObject(i).getJSONObject("geometry")
+                        val coordinateArray:JSONArray = geometryObj.getJSONArray("coordinates")
+                            val firstCoordinate = coordinateArray.getDouble(0)
+                            Log.d("First Coordinate: ", firstCoordinate.toString())
+                    }
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { error: VolleyError ->
+                try {
+                    Log.d("Error: ", error.toString())
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            })
+
+        volleyRequest!!.add(jsonBigRequest)
+    }
+
     //This is for a json object that has an array of objects
-    private fun parseJson(Url: String) {
+    private fun parseJsonObject(Url: String) {
 
         //val url ="https://pixabay.com/api/?key=9900098-8e28a903a63da19e7d8c5ab7a&q=nude+female&image_type=photo&pretty=true"
 
         val request = JsonObjectRequest(Request.Method.GET, Url, null,
             Response.Listener { response ->
                 try {
-                   val jsonArray: JSONArray = response.getJSONArray("hits")
-                    for (i in 0 until jsonArray.length()){
+                    val jsonArray: JSONArray = response.getJSONArray("hits")
+                    for (i in 0 until jsonArray.length()) {
                         val hit: JSONObject = jsonArray.optJSONObject(i)
                         var creatorName: String = hit.getString("user")
                         var imageUrl: String = hit.getString("webformatURL")
@@ -49,31 +86,50 @@ class MainActivity : AppCompatActivity() {
                         //Log.d("Array Entry " + i, jsonArray.toString())
                         Log.d("Array Entry $i", creatorName + "\n" + imageTags + "\n" + imageUrl)
                     }
-                }catch (e: JSONException){e.printStackTrace()}
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
             },
             Response.ErrorListener { error: VolleyError? ->
                 try {
                     Log.d("XXX ERROR XXX", error.toString())
 
-                }catch (e: JSONException){e.printStackTrace()}
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
             })
         volleyRequest!!.add(request)
 
     }
 
     //This is for a json Array of objects
-    private fun parseArray(Url: String){
+    private fun parseJsonArray(Url: String) {
 
         val jsonArray = JsonArrayRequest(Request.Method.GET, Url, null,
             Response.Listener { response: JSONArray ->
                 try {
                     Log.d("<---- Response ---->", response.toString())
 
-                }catch (e: JSONException){
+                    for (i in 0 until response.length()) {
+                        val userObj = response.getJSONObject(i)
+
+                        val userName = userObj.getString("username")
+                        val userMail = userObj.getString("email")
+
+                        Log.d("UserName ", "$userName\nUserMail $userMail")
+                    }
+
+                } catch (e: JSONException) {
                     e.printStackTrace()
                 }
             },
-            Response.ErrorListener {  })
+            Response.ErrorListener { error: VolleyError ->
+                try {
+                    Log.e("Error: ", error.toString())
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            })
 
         volleyRequest!!.add(jsonArray)
 
